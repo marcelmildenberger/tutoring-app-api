f065739a47404375867afac79229f836b82e03a5
Mildem122 <marcel.milde@outlook.de>
2022-12-01 10:31:24 +0100

    firebase cloud functions

--- index.mjs
+++ index.mjs
@@ -1,26 +1,27 @@
 import express from "express";
+import { db } from "./firebase.mjs";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  query,
+  setDoc,
+  where,
+  getDocs,
+  getDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get("/tshirt", (req, res) => {
-  res.status(200).send({
-    tshirt: "cool",
-    size: "large",
-  });
-});
+app.get("/getUser/:uid", async (req, res) => {
+  const { uid } = req.params;
+  const user = await getDoc(doc(db, "users", uid));
+  console.log(user);
 
-app.post("/tshirt/:id", (req, res) => {
-  const { id } = req.params;
-  const { logo } = req.body;
-
-  if (!logo) {
-    res.status(418).send({ message: "We need a logo!" });
-  }
-  res.send({
-    tshirt: `tshirt with your ${logo} and ID of ${id}`,
-  });
+  return res.status(200);
 });
 
 app.listen(PORT, () => console.log(`Its alive on http://localhost:${PORT}`));
